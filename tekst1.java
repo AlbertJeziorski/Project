83adf28f22227ef84fe221c218dac96688f765bc
AlbertJeziorski <jeziorski.albert@gmail.com>
2023-06-22 23:56:27 +0200

    dodanie kolejnych treści

--- tekst1.java
+++ tekst1.java
@@ -4,6 +4,6 @@
 public class tekst1 {
 
     public static void main(String[] args) {
-        System.out.println("hello ");
+        System.out.println("are u okay? ");
     }
 }
--- tekst2.java
+++ tekst2.java
@@ -4,6 +4,6 @@
 public class tekst2 {
 
     public static void main(String[] args) {
-        System.out.println("u dont know ?");
+        System.out.println("ah I remember now sory");
     }
 }
